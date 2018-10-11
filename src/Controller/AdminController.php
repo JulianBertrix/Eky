c90f5f3259fc6aada53f3ab7a9628d6c873e42ad
Julian Bertrix <j.bertrix@gmail.com>
2018-10-11 16:04:55 +0200

    creation formulaire admin

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\TypeDechet;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index()
+    {
+        $types = $this->getDoctrine()->getRepository(TypeDechet::class)->findAll();
+        
+        return $this->render('admin/index.html.twig', [
+            'types' => $types,
+        ]);
+    }
+}
