ee4e7372a1a003c286f7b8ac4189457694ba6de0
Julian Bertrix <j.bertrix@gmail.com>
2018-10-12 09:06:27 +0200

    modif

--- src/Controller/ApiFormulaireAdminController.php
+++ src/Controller/ApiFormulaireAdminController.php
@@ -9,6 +9,7 @@ use App\Entity\Particulier;
 use App\Entity\Dechet;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ApiFormulaireAdminController extends AbstractController
 {
@@ -18,24 +19,24 @@ class ApiFormulaireAdminController extends AbstractController
      */
     public function create(Request $request)
     {
-//        $post = $request->request->all();
-//        
-//        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(["user_code"=>$post['user']]);
-//        $particulier = $this->getDoctrine()->getRepository(Particulier::class)->findOneBy(['user_id'=>$user->getId()]);
-//        $type = $this->getDoctrine()->getRepository(TypeDechet::class)->find($post['type']);
-//        
-//        $dechet = new Dechet();
-//        $dechet->setQuantiteUtilise(0);
-//        $dechet->setQuantite($post['qte']);
-//        $dechet->setTypeDechetId($this->getDoctrine()->getManager()->find(TypeDechet::class, $type->getId()));
-//        $dechet->setParticulierId($this->getDoctrine()->getManager()->find(Particulier::class, $particulier->getId()));
-//        $dechet->setDate(date('Y-m-d'));
-//        $this->getDoctrine()->getManager()->persist($dechet);
-//        $this->getDoctrine()->getManager()->flush();
+        $post = $request->request->all();
+        
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(["user_code"=>$post['user']]);
+        $particulier = $this->getDoctrine()->getRepository(Particulier::class)->findOneBy(['user_id'=>$user->getId()]);
+        $type = $this->getDoctrine()->getRepository(TypeDechet::class)->find($post['type']);
+        
+        $dechet = new Dechet();
+        $dechet->setQuantiteUtilise(0);
+        $dechet->setQuantite($post['qte']);
+        $dechet->setTypeDechetId($this->getDoctrine()->getManager()->find(TypeDechet::class, $type->getId()));
+        $dechet->setParticulierId($this->getDoctrine()->getManager()->find(Particulier::class, $particulier->getId()));
+        $dechet->setDate(date('Y-m-d'));
+        $this->getDoctrine()->getManager()->persist($dechet);
+        $this->getDoctrine()->getManager()->flush();
         
         $typeDechets = $this->getDoctrine()->getRepository(TypeDechet::class)->findAll();
-        $retour = $this->json(JSON_encode($typeDechets)); 
-        //print_r($retour);
-        return new \Symfony\Component\HttpFoundation\JsonResponse(['data' => $typeDechets]);
+        //$retour = $this->json(JSON_encode($typeDechets)); 
+        
+        return new JsonResponse(['data' => $typeDechets]);
     }
 }
