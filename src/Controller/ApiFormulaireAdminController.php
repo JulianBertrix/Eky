a067b6d385d569dd5d2cc925afa756bbe858c5e8
Unknown <j.bertrix@gmail.com>
2018-10-12 10:45:43 +0200

    Merge branch 'api_admin' into merge

---
+++ src/Controller/ApiFormulaireAdminController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\TypeDechet;
+use App\Entity\Particulier;
+use App\Entity\Dechet;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiFormulaireAdminController extends AbstractController
+{
+    /**
+     * @Route("/api/formulaire/dechet/add", name="api_formulaire_admin")
+     * @Method({"POST"})
+     */
+    public function create(Request $request)
+    {
+        $post = $request->request->all();
+        
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(["user_code"=>$post['user']]);
+        $particulier = $this->getDoctrine()->getRepository(Particulier::class)->findOneBy(['user_id'=>$user->getId()]);
+        $type = $this->getDoctrine()->getRepository(TypeDechet::class)->find($post['type']);
+        
+        $dechet = new Dechet();
+        $dechet->setQuantiteUtilise(0);
+        $dechet->setQuantite($post['qte']);
+        $dechet->setTypeDechetId($this->getDoctrine()->getManager()->find(TypeDechet::class, $type->getId()));
+        $dechet->setParticulierId($this->getDoctrine()->getManager()->find(Particulier::class, $particulier->getId()));
+        $dechet->setDate(date('Y-m-d'));
+        $this->getDoctrine()->getManager()->persist($dechet);
+        $this->getDoctrine()->getManager()->flush();
+        
+        $typeDechets = $this->getDoctrine()->getRepository(TypeDechet::class)->findAll();
+        //$retour = $this->json(JSON_encode($typeDechets)); 
+        
+        return new JsonResponse(['data' => $typeDechets]);
+    }
+}
